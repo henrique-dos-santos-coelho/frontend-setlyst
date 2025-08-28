03d00a9bf64d1c01f5212588e6106e3f4b374f78
Henrique Coelho <rike.coelho22@gmail.com>
2025-08-28 14:41:58 -0300

    init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  </main>
+</template>
